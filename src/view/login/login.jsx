b983d3b1132b30f1893022ea637a916376bfa6b4
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2025-07-31 16:44:40 -0300

    tela de login

--- src/view/login/login.jsx
+++ src/view/login/login.jsx
@@ -1,9 +1,31 @@
-import './login.style.css'
+import "./login.style.css";
+import digital from "../../assets/dig.png";
 
 function Login() {
   return (
-    <div>login</div>
-  )
+      <div className="login-card">
+        <div className="fingerprint-icon">
+          <img src={digital} alt="Ícone de impressão digital" />
+        </div>
+        <h1>Acessar minha conta.</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">E-MAIL</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div class="form-group">
+            <label htmlFor="senha">SENHA</label>
+            <input type="password" id="senha" name="senha" required />
+          </div>
+          <button type="submit" className="login-button">
+            ACESSAR
+          </button>
+        </form>
+        <a href="#" className="signup-link">
+          Não tenho uma conta. Criar agora!
+        </a>
+      </div>
+  );
 }
 
-export default Login
+export default Login;
