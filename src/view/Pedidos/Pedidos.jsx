85faee69de6f9d562222f9f026ae9082f50bfa2c
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2025-08-13 17:01:53 -0300

    Pag Pedidos

--- src/view/Pedidos/Pedidos.jsx
+++ src/view/Pedidos/Pedidos.jsx
@@ -2,6 +2,7 @@ import MenuLateral from "../../components/MenuLateral/MenuLateral";
 
 import "./pedidos.style.css";
 import CustomButtom from "../../components/CustomButtom/CustomButtom";
+import TotalPedidos from "./Graficos/TotalPedidos";
 
 function Pedidos() {
   return (
@@ -13,6 +14,27 @@ function Pedidos() {
         <div className="topPedidos">
           <h2 className="TitlePedidos">Análise de Pedidos</h2>
           <CustomButtom texto={'+ Pedido'}/>
+        </div>
+        <div className="pesquisaPedidos">
+          <input className="" placeholder="Pesquisar pedidos..." type="text"/>
+          <CustomButtom texto={'Buscar'}/>
+        </div>
+        <div className="GraficosPedidos">
+          <div className="TotalPedidos">
+            <h4>Total Pedidos</h4>
+            <h3>1.365</h3>
+            <TotalPedidos />
+          </div>
+          <div className="TotalPedidos">
+            <h4>Receita Total</h4>
+            <h3>1.365</h3>
+            <TotalPedidos />
+          </div>
+          <div className="TotalPedidos">
+            <h4>Pedidos Pendentes</h4>
+            <h3>1.365</h3>
+            <TotalPedidos />
+          </div>
         </div>
 
       </div>
